fa1abcd347cee163f74cd4f7ed2243381d814419
juhyepyoun <73421788+juhyepyoun@users.noreply.github.com>
2023-03-14 20:26:52 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        answer += hp / 5;
+        if(hp % 5 != 0) {
+            hp %= 5;
+            answer += hp / 3;
+            if(hp % 3 != 0) {
+                hp %= 3;
+                answer += hp;
+                return answer;
+            } else {
+                return answer;
+            }
+        } else {
+            return answer;
+        }
+    }
+}
