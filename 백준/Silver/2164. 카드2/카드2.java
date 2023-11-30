10705dc56b3e172b266c212e6bd0acf1b4c9957e
Juhye Pyoun <73421788+juhyepyoun@users.noreply.github.com>
2023-11-30 15:39:58 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 53364 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+        
+        while(queue.size() > 1) {
+            queue.poll();
+            if(queue.size() == 1) {
+                break;
+            }
+            int top = queue.poll();
+            queue.offer(top);
+        }
+        
+        System.out.println(queue.peek());
+    }
+}
